94723e5374ba8987b41e91dcf1ddb112bb010da2
Guilherme da Silva Tesch <tesch.guilherme@hotmail.com>
2025-06-17 21:39:59 -0300

    Modificando a classe Main para declarar a variavel lista como a interface List, demonstrando o uso de polimorfismo.

--- src/com/guilherme/tesch/principal/Main.java
+++ src/com/guilherme/tesch/principal/Main.java
@@ -6,17 +6,17 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args){
+        List<String> listaPolimorfica;
 
+        listaPolimorfica = new ArrayList<>();
+        listaPolimorfica.add("Elemento 1");
+        listaPolimorfica.add("Elemento 2");
 
-        List<String> listaArrayList = new ArrayList<>();
-        listaArrayList.add("Elemento 1");
-        listaArrayList.add("Elemento 2");
+        listaPolimorfica = new LinkedList<>();
+        listaPolimorfica.add("Elemento A");
+        listaPolimorfica.add("Elemento B");
 
-        List<String> listaLinkedList = new LinkedList<>();
-        listaLinkedList.add("Elemento A");
-        listaLinkedList.add("Elemento B");
-
-        System.out.println("ArrayList: "+ listaArrayList);
-        System.out.println("LinkedList: "+ listaLinkedList);
+        System.out.println("ArrayList: "+ listaPolimorfica);
+        System.out.println("LinkedList: "+ listaPolimorfica);
     }
 }
